016c38d8b0957ddb9b0939c0686411cb569a0028
Harun1804 <Harun.arrasyid1804@gmail.com>
2022-11-16 09:22:17 +0700

    simple discord notif

---
+++ app/Http/Controllers/SendNotifDiscord.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class SendNotifDiscord extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        return Http::post(config('notification.discord.hook_url'),[
+            'content'   => "Learning how to send notifications from laravel to discord",
+            'embeds'    => [
+                [
+                    'title' => 'An Awesome',
+                    'description' => 'Discord webhooks are great',
+                    'color' => '7506394'
+                ]
+            ]
+        ]);
+    }
+}
---
+++ config/notification.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'discord' => [
+        'hook_url' => env('DISCORD_WEB_HOOK')
+    ]
+];
